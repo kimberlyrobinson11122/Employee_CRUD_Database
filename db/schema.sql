2082a84435a2d71968e7cba55b64e3376bfbaacd
Kimberly Robinson <158204628+kimberlyrobinson11122@users.noreply.github.com>
2024-05-19 19:26:39 -0700

    fixed ids and issues

---
+++ db/schema.sql
@@ -0,0 +1,34 @@
+-- Drop the existing database if it exists (refreshes the entire database)
+DROP DATABASE IF EXISTS employee_db;
+
+-- Then create the database
+CREATE DATABASE employee_db;
+
+-- Select/use the database
+USE employee_db;
+
+-- This creates the department table with a primary key
+CREATE TABLE department (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  department_name VARCHAR(30) NOT NULL
+);
+
+-- This creates the role table with a primary key and foreign key referencing the department table
+CREATE TABLE role (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL(10, 2) NOT NULL,
+  department_id INT NOT NULL,
+  FOREIGN KEY (department_id) REFERENCES department(id)
+);
+
+-- This creates the employee table with a primary key and foreign keys referencing role and department tables
+CREATE TABLE employee (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT NOT NULL,
+  manager_id INT,
+  FOREIGN KEY (role_id) REFERENCES role(id),
+  FOREIGN KEY (manager_id) REFERENCES employee(id)
+);
