ac12d9b8af3e941a7d1ed4a98a048a02b02c32df
Kimberly Robinson <158204628+kimberlyrobinson11122@users.noreply.github.com>
2024-06-08 14:20:32 -0700

    updated

--- db/query.sql
+++ db/query.sql
@@ -1,7 +1,9 @@
-SELECT employee.employee_name AS employee,
-       role.title AS role,
-       department.department_name AS department
+SELECT employee_db_table
+    employee.first_name AS first_name,
+    employee.last_name AS last_name,
+    role.title AS role,
+    department.department_name AS department
 FROM employee
-LEFT JOIN role ON employee.role_id = role.role_id
-LEFT JOIN department ON employee.department_id = department.department_id
-ORDER BY department.department_name, role.title, employee.employee_name;
+LEFT JOIN role ON employee.role_id = role_id
+LEFT JOIN department ON role.department_id = department_id
+ORDER BY department.department_name, role.title, employee_id;
